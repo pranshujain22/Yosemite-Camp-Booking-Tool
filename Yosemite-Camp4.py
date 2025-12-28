0d58f7da496e6ddbd4147759898881793aefae49
Pranshu Jain <pranshu.jain22@gmail.com>
2025-12-28 14:59:59 -0600

    Update Yosemite-Camp4.py

--- Yosemite-Camp4.py
+++ Yosemite-Camp4.py
@@ -1,3 +1,4 @@
+import json
 import random
 import time
 
@@ -14,18 +15,29 @@ def human_sleep(a=0.2, b=0.5):
     time.sleep(random.uniform(a, b))
 
 
+def load_config(file_path='config.json'):
+    """Loads configuration details from a JSON file."""
+    try:
+        with open(file_path, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f"Error: {file_path} not found. Please create it.")
+        exit(1)
+
+
 def run(driver):
     try:
+        config = load_config()
         driver.get("https://www.recreation.gov/camping/campgrounds/10004152")
         wait = WebDriverWait(driver, 10)
         lang_button = wait.until(EC.presence_of_element_located((By.ID, "ga-global-nav-log-in-link")))
         lang_button.click()
 
         email = driver.find_element(By.ID, "email")
-        email.send_keys("pranshu.jain22@gmail.com")
+        email.send_keys(config["email"])
 
         password = driver.find_element(By.ID, "rec-acct-sign-in-password")
-        password.send_keys("Jarvis@1997")
+        password.send_keys(config["password"])
         human_sleep()
 
         login_button = driver.find_element(By.XPATH, "//button[.//span[text()='Log In']]")
@@ -37,13 +49,8 @@ def run(driver):
         wait = WebDriverWait(driver, 10)
         table = wait.until(EC.presence_of_element_located((By.ID, "availability-table")))
 
-        # next_btn = driver.find_element(By.XPATH, '//button[@aria-label="Go Forward 5 Days"]')
-        # next_btn.click()
-        # human_sleep()
-        # next_btn.click()
-
         camp_site = driver.find_element(By.XPATH,
-                                        '//button[contains(@aria-label, "Sep 13, 2025 - Site Camp 4 is available")]')
+                                        f'//button[contains(@aria-label, "{config["date"]} - Site Camp 4 is available")]')
         driver.execute_script("arguments[0].click();", camp_site)
 
         human_sleep()
@@ -53,7 +60,7 @@ def run(driver):
 
         group_size = driver.find_element(By.XPATH, '//input[contains(@id, "per-person-number-field-10046502")]')
         driver.execute_script("arguments[0].value = '';", group_size)
-        group_size.send_keys("5")
+        group_size.send_keys(config["group_size"])
 
         book_button = driver.find_element(By.XPATH, "//button[.//span[text()='Book']]")
         book_button.click()
@@ -71,37 +78,20 @@ def run(driver):
         url = driver.current_url
         booking_id = url.split("id=")[-1]
 
-        first_name_map = {
-            1: "Shyamal",
-            2: "Vishal",
-            3: "Minni",
-            4: "Kashish",
-            5: "Fnu"
-        }
-
-        last_name_map = {
-            1: "Jadav",
-            2: "Upadhyay",
-            3: "Jajodia",
-            4: "Yadav",
-            5: "Akhil",
-        }
-
         human_sleep()
 
-        for index in range(1, 5):
+        for index in range(1, config["group_size"]):
             wait = WebDriverWait(driver, 10)
             first_name = wait.until(EC.element_to_be_clickable((By.ID, f'first_name-{booking_id}-{index}')))
             driver.execute_script("arguments[0].scrollIntoView(true);", first_name)
-            first_name.send_keys(first_name_map[index])
+            first_name.send_keys(config['group_details'][index]['first_name'])
 
             last_name = driver.find_element(By.ID, f'last_name-{booking_id}-{index}')
-            # driver.execute_script("arguments[0].value = '" + last_name_map[index] + "';", last_name)
-            last_name.send_keys(last_name_map[index])
+            last_name.send_keys(config['group_details'][index]['last_name'])
 
             postal_code = driver.find_element(By.ID, f'postal_code-{booking_id}-{index}')
             # driver.execute_script("arguments[0].value = '75039';", postal_code)
-            postal_code.send_keys("75039")
+            postal_code.send_keys(config['group_details'][index]['postal_code'])
 
             human_sleep()
 
@@ -160,17 +150,17 @@ def run(driver):
         wait = WebDriverWait(driver, 10)
         first_name = wait.until(EC.element_to_be_clickable((By.ID, f'first_name-{booking_id}-5')))
         driver.execute_script("arguments[0].scrollIntoView(true);", first_name)
-        driver.execute_script("arguments[0].value = '" + first_name_map[5] + "';", first_name)
+        driver.execute_script("arguments[0].value = '" + config['group_details'][-1]['first_name'] + "';", first_name)
         first_name.send_keys(Keys.DELETE)
         first_name.send_keys(Keys.BACKSPACE)
 
         last_name = driver.find_element(By.ID, f'last_name-{booking_id}-5')
-        driver.execute_script("arguments[0].value = '" + last_name_map[5] + "';", last_name)
+        driver.execute_script("arguments[0].value = '" + config['group_details'][-1]['last_name'] + "';", last_name)
         last_name.send_keys(Keys.DELETE)
         last_name.send_keys(Keys.BACKSPACE)
 
         postal_code = driver.find_element(By.ID, f'postal_code-{booking_id}-5')
-        driver.execute_script("arguments[0].value = '75039';", postal_code)
+        driver.execute_script(f"arguments[0].value = '{config['group_details'][-1]['postal_code']}';", postal_code)
         postal_code.send_keys(Keys.DELETE)
         postal_code.send_keys(Keys.BACKSPACE)
 
